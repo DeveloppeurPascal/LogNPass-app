9841ac8a88288e42ff838f5bd16a0c724cbe7d13
Patrick PREMARTIN <patrick.premartin@developpeur-pascal.fr>
2023-02-25 21:05:00 +0100

    added project images

---
+++ src/dm.pas
@@ -0,0 +1,26 @@
+unit dm;
+
+interface
+
+uses
+  System.SysUtils, System.Classes, System.ImageList, FMX.ImgList;
+
+type
+  TDataModule1 = class(TDataModule)
+    ImageList1: TImageList;
+  private
+    { Déclarations privées }
+  public
+    { Déclarations publiques }
+  end;
+
+var
+  DataModule1: TDataModule1;
+
+implementation
+
+{%CLASSGROUP 'FMX.Controls.TControl'}
+
+{$R *.dfm}
+
+end.
