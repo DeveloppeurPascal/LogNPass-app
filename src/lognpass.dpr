221d065cbd8fb50f150b105be2210f7566d57011
Patrick PREMARTIN <patrick.premartin@developpeur-pascal.fr>
2024-05-06 18:54:32 +0200

    updated file paths for DeveloppeurPascal/Librairies units

--- src/lognpass.dpr
+++ src/lognpass.dpr
@@ -6,9 +6,9 @@ uses
   frm_principale in 'frm_principale.pas' {frmMain},
   u_ajax in 'u_ajax.pas',
   u_lognpass in '..\lib-externes\LogNPass-Delphi\src\u_lognpass.pas',
-  u_md5 in '..\lib-externes\librairies\u_md5.pas',
   dm in 'dm.pas' {DataModule1: TDataModule},
-  u_urlOpen in '..\lib-externes\librairies\u_urlOpen.pas';
+  u_urlOpen in '..\lib-externes\librairies\src\u_urlOpen.pas',
+  u_md5 in '..\lib-externes\librairies\src\u_md5.pas';
 
 {$R *.res}
 
